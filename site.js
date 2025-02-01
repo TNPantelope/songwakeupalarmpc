6f2f64792a187284b7e72d31020a04f01fd113c4
Tamim Norani <tamimn@student.uia.no>
2025-02-01 13:03:17 +0100

    fixed the hours to show as double digits, 01 instead of 1

--- site.js
+++ site.js
@@ -4,7 +4,12 @@ let clock = document.querySelector('.clock')
 
 function updateTime() {
     let currentTime = new Date();
-    clock.innerHTML = currentTime.toLocaleTimeString('en-US');
+    clock.innerHTML = currentTime.toLocaleTimeString('en-US', {
+        hour12: true,
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    });
     setTimeout(updateTime, 1000);
 }
 updateTime();
