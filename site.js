dfe4d66ef1dd482ce882c037847b094cc6ca2358
Tamim Norani <tamimn@student.uia.no>
2025-02-01 12:47:18 +0100

    fixed digital clock

--- site.js
+++ site.js
@@ -3,19 +3,8 @@
 let clock = document.querySelector('.clock')
 
 function updateTime() {
-    let hours = new Date().getHours();
-    let minutes = new Date().getMinutes();
-    let seconds = new Date().getSeconds();
-
-    hours = hours < 10 ? "0" + hours : hours;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-
-    clock.innerHTML = `${hours}:${minutes}:${seconds}`;
-
-    // refreshes the clock each second
+    let currentTime = new Date();
+    clock.innerHTML = currentTime.toLocaleTimeString('en-US');
     setTimeout(updateTime, 1000);
 }
 updateTime();
-
