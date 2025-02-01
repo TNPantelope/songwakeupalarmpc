3d8c253da0e9789a0f16b8f473021cdefeef7b55
Tamim Norani <tamimn@student.uia.no>
2025-02-01 13:54:41 +0100

    changed the time to 12 hour format and removed seconds, also made am/pm smaller and put it in the bottom right corner of the clock. Also got started on the song container

--- site.js
+++ site.js
@@ -1,15 +1,18 @@
 
 
 let clock = document.querySelector('.clock')
-
 function updateTime() {
     let currentTime = new Date();
-    clock.innerHTML = currentTime.toLocaleTimeString('en-US', {
+    let timeString = currentTime.toLocaleTimeString('en-US', {
         hour12: true,
         hour: '2-digit',
         minute: '2-digit',
-        second: '2-digit'
+        //second: '2-digit',
     });
+    
+    
+    let [time, meridiem] = timeString.split(' ');
+    clock.innerHTML = `${time}<span class="meridiem">${meridiem.toLowerCase()}</span>`;
     setTimeout(updateTime, 1000);
 }
 updateTime();
